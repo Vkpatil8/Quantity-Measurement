7f9d12c6a2034345504717f18d44554b233efd50
vkpatil <vishalvkpatil8@gmail.com>
2022-02-05 23:59:09 +0530

    [vishal] Updated: test case for check type

--- quantity_measurement.py
+++ quantity_measurement.py
@@ -2,23 +2,32 @@
 @Author: Vishal Patil
 @Date: 05-02-2022 11-00-00
 @Last Modified by: Vishal Patil
-@Last Modified time: 05-02-2022 22:20:00
-@Title : solve test case 1.2
+@Last Modified time: 05-02-2022 23:30:00
+@Title : solve test case 1.3
 """
 from measurement_exception import MeasurementException
 
 
 class QuantityMeasurement:
     @staticmethod
-    def equalLength(a):
-
-        if a is not None:
-            return a
+    def compareValue(a):
+        if a == 0:
+            return 0
         else:
-            raise MeasurementException("Null")
+            raise MeasurementException("Not Null")
+
+    @staticmethod
+    def equalLength(a):
+        if a is None:
+            return True
+        else:
+            raise MeasurementException("Not Null")
 
     @staticmethod
     def typeCheck(a, b):
-        if type(a) != type(b):
-            raise MeasurementException("Not equal")
-        return True
+        if type(a) == type(b):
+            return True
+        else:
+            raise MeasurementException("Not equal type")
+
+
--- test/test.py
+++ test/test.py
@@ -1,6 +1,4 @@
-import pytest
 
-from measurement_exception import MeasurementException
 from quantity_measurement import QuantityMeasurement
 
 
@@ -12,25 +10,23 @@ class Test:
 
         """
         expected = 0
-        first_obj = QuantityMeasurement.equalLength(0)
+        first_obj = QuantityMeasurement.compareValue(0)
         assert first_obj == expected
 
-    def test_raise_exception_on_null_value(self):
+    def test_raise_exception_on_not_null_value(self):
         """
             desc: check object using equal method having null value in one object and same unit
         """
-        with pytest.raises(MeasurementException) as exception:
-            expected = None
-            first_obj = QuantityMeasurement.equalLength(None)
-            first_obj == expected
-        assert exception.value.message == "Null"
+        expected = True
+        first_obj = QuantityMeasurement.equalLength(None)
+        assert first_obj == expected
 
     def test_raise_exception_on_different_type(self):
         """
             desc: check two object using equal method having type of one object equal to another
         """
-        with pytest.raises(MeasurementException) as exception:
-            first_obj = QuantityMeasurement.typeCheck("a", 0)
-            expected = True
-            first_obj == expected
-        assert exception.value.message == "Not equal"
+        first_obj = QuantityMeasurement.typeCheck("a", "a")
+        expected = True
+        assert first_obj == expected
+
+
