c65066a2b613c952c710e4ad86b18a4625b930f7
vkpatil <vishalvkpatil8@gmail.com>
2022-02-06 13:53:16 +0530

    [vishal] Updated: test case for check equality of value

--- quantity_measurement.py
+++ quantity_measurement.py
@@ -2,7 +2,7 @@
 @Author: Vishal Patil
 @Date: 05-02-2022 11-00-00
 @Last Modified by: Vishal Patil
-@Last Modified time: 05-02-2022 24:00:00
+@Last Modified time: 06-02-2022 13:30:00
 @Title : solve test case 1.4
 """
 from measurement_exception import MeasurementException
@@ -13,14 +13,9 @@ class QuantityMeasurement:
         self.unit = unit
         self.length = length
 
-    def convert(self, unit, length):
-        return unit * length
-
     def __eq__(self, other):
         if self.length is None or other.length is None:
             raise MeasurementException("Null Value")
-        if self.unit != other.unit and other.length != self.length:
-            return self.convert(self.unit, self.length) == self.convert(other.unit, other.length)
         if self.unit != other.unit and other.length == self.length:
             raise MeasurementException("Different units don't have same length")
         if type(self.length) != type(other.length):
