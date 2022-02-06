418f56dc675e37e14a02bab68329df60f1787b76
vkpatil <vishalvkpatil8@gmail.com>
2022-02-06 17:09:59 +0530

    [vishal] Added: refactore code

---
+++ inch.py
@@ -0,0 +1,2 @@
+class Inch:
+    INCH = 1
--- test/test.py
+++ test/test.py
@@ -1,45 +1,58 @@
+import pytest
+
+from feet import Feet
+from inch import Inch
 from quantity_measurement import QuantityMeasurement
 
 
 class Test:
-
-    def test_compare_two_having_zero_values(self):
+    @pytest.mark.parametrize('first_unit, first_length, second_unit, second_length',
+                             [(Feet.FEET, 0, Feet.FEET, 0), (Inch.INCH, 0, Inch.INCH, 0)])
+    def test_compare_two_having_zero_values(self, first_unit, first_length, second_unit, second_length):
         """
             desc: check two object using equal method having zero length
 
         """
-        first_obj = QuantityMeasurement("Feet", 0)
-        second_obj = QuantityMeasurement("Feet", 0)
+        first_obj = QuantityMeasurement(first_unit, first_length)
+        second_obj = QuantityMeasurement(second_unit, second_length)
         assert first_obj == second_obj
 
-    def test_raise_exception_on_null_value(self):
+    @pytest.mark.parametrize('first_unit, first_length, second_unit, second_length',
+                             [(Feet.FEET, 1.0, Inch.INCH, 12.0), (Inch.INCH, 12.0, Feet.FEET, 1.0)])
+    def test_raise_exception_on_null_value(self, first_unit, first_length, second_unit, second_length):
         """
             desc: check object using equal method having null value in one object and same unit
         """
-        first_obj = QuantityMeasurement("Feet", 1)
-        second_obj = QuantityMeasurement("Feet", 1)
+        first_obj = QuantityMeasurement(first_unit, first_length)
+        second_obj = QuantityMeasurement(second_unit, second_length)
         assert first_obj == second_obj
 
-    def test_raise_exception_on_different_type_values(self):
+    @pytest.mark.parametrize('first_unit, first_length, second_unit, second_length',
+                             [(Feet.FEET, 1.0, Inch.INCH, 12.0), (Inch.INCH, 12.0, Feet.FEET, 1.0)])
+    def test_raise_exception_on_different_type_values(self, first_unit, first_length, second_unit, second_length):
         """
             desc: check object using equal method having type of one object and same unit
         """
-        first_obj = QuantityMeasurement("Feet", 0)
-        second_obj = QuantityMeasurement("Feet", 0)
+        first_obj = QuantityMeasurement(first_unit, first_length)
+        second_obj = QuantityMeasurement(second_unit, second_length)
         assert first_obj == second_obj
 
-    def test_raise_exception_on_different_value(self):
+    @pytest.mark.parametrize('first_unit, first_length, second_unit, second_length',
+                             [(Feet.FEET, 1.0, Inch.INCH, 12.0), (Inch.INCH, 12.0, Feet.FEET, 1.0)])
+    def test_raise_exception_on_different_value(self, first_unit, first_length, second_unit, second_length):
         """
             desc: check two object using equal method having value of one object equal to another
         """
-        first_obj = QuantityMeasurement("Feet", 1)
-        second_obj = QuantityMeasurement("Feet", 1)
+        first_obj = QuantityMeasurement(first_unit, first_length)
+        second_obj = QuantityMeasurement(second_unit, second_length)
         assert first_obj == second_obj
 
-    def test_for_checking_reference(self):
+    @pytest.mark.parametrize('first_unit, first_length, second_unit, second_length',
+                             [(Feet.FEET, 1.0, Inch.INCH, 12.0), (Inch.INCH, 12.0, Feet.FEET, 1.0)])
+    def test_for_checking_reference(self, first_unit, first_length, second_unit, second_length):
         """
             desc: check object using equal method have same reference
         """
-        first_obj = QuantityMeasurement("Feet", 0)
-        second_obj = QuantityMeasurement("Feet", 0)
+        first_obj = QuantityMeasurement(first_unit, first_length)
+        second_obj = QuantityMeasurement(second_unit, second_length)
         assert first_obj == second_obj
