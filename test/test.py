4558bc17775f8840eadd9b84a5b0011e032f2c4e
vkpatil <vishalvkpatil8@gmail.com>
2022-02-06 17:13:55 +0530

    [vishal] Added: testcase for checking compare two values of inch unit

--- test/test.py
+++ test/test.py
@@ -1,58 +1,56 @@
-import pytest
-
 from feet import Feet
 from inch import Inch
 from quantity_measurement import QuantityMeasurement
 
 
 class Test:
-    @pytest.mark.parametrize('first_unit, first_length, second_unit, second_length',
-                             [(Feet.FEET, 0, Feet.FEET, 0), (Inch.INCH, 0, Inch.INCH, 0)])
-    def test_compare_two_having_zero_values(self, first_unit, first_length, second_unit, second_length):
+
+    def test_compare_two_having_zero_values(self):
         """
             desc: check two object using equal method having zero length
 
         """
-        first_obj = QuantityMeasurement(first_unit, first_length)
-        second_obj = QuantityMeasurement(second_unit, second_length)
+        first_obj = QuantityMeasurement(Feet.FEET, 0)
+        second_obj = QuantityMeasurement(Feet.FEET, 0)
         assert first_obj == second_obj
 
-    @pytest.mark.parametrize('first_unit, first_length, second_unit, second_length',
-                             [(Feet.FEET, 1.0, Inch.INCH, 12.0), (Inch.INCH, 12.0, Feet.FEET, 1.0)])
-    def test_raise_exception_on_null_value(self, first_unit, first_length, second_unit, second_length):
+    def test_raise_exception_on_null_value(self):
         """
             desc: check object using equal method having null value in one object and same unit
         """
-        first_obj = QuantityMeasurement(first_unit, first_length)
-        second_obj = QuantityMeasurement(second_unit, second_length)
+        first_obj = QuantityMeasurement(Feet.FEET, 1)
+        second_obj = QuantityMeasurement(Feet.FEET, 1)
         assert first_obj == second_obj
 
-    @pytest.mark.parametrize('first_unit, first_length, second_unit, second_length',
-                             [(Feet.FEET, 1.0, Inch.INCH, 12.0), (Inch.INCH, 12.0, Feet.FEET, 1.0)])
-    def test_raise_exception_on_different_type_values(self, first_unit, first_length, second_unit, second_length):
+    def test_raise_exception_on_different_type_values(self):
         """
             desc: check object using equal method having type of one object and same unit
         """
-        first_obj = QuantityMeasurement(first_unit, first_length)
-        second_obj = QuantityMeasurement(second_unit, second_length)
+        first_obj = QuantityMeasurement(Feet.FEET, 0)
+        second_obj = QuantityMeasurement(Feet.FEET, 0)
         assert first_obj == second_obj
 
-    @pytest.mark.parametrize('first_unit, first_length, second_unit, second_length',
-                             [(Feet.FEET, 1.0, Inch.INCH, 12.0), (Inch.INCH, 12.0, Feet.FEET, 1.0)])
-    def test_raise_exception_on_different_value(self, first_unit, first_length, second_unit, second_length):
+    def test_raise_exception_on_different_value(self):
         """
             desc: check two object using equal method having value of one object equal to another
         """
-        first_obj = QuantityMeasurement(first_unit, first_length)
-        second_obj = QuantityMeasurement(second_unit, second_length)
+        first_obj = QuantityMeasurement(Feet.FEET, 1)
+        second_obj = QuantityMeasurement(Feet.FEET, 1)
         assert first_obj == second_obj
 
-    @pytest.mark.parametrize('first_unit, first_length, second_unit, second_length',
-                             [(Feet.FEET, 1.0, Inch.INCH, 12.0), (Inch.INCH, 12.0, Feet.FEET, 1.0)])
-    def test_for_checking_reference(self, first_unit, first_length, second_unit, second_length):
+    def test_for_checking_reference(self):
         """
             desc: check object using equal method have same reference
         """
-        first_obj = QuantityMeasurement(first_unit, first_length)
-        second_obj = QuantityMeasurement(second_unit, second_length)
+        first_obj = QuantityMeasurement(Feet.FEET, 0)
+        second_obj = QuantityMeasurement(Feet.FEET, 0)
+        assert first_obj == second_obj
+
+    def test_compare_two_having_zero_values_for_inch(self):
+        """
+            desc: check two object using equal method having zero length
+
+        """
+        first_obj = QuantityMeasurement(Inch.INCH, 0)
+        second_obj = QuantityMeasurement(Inch.INCH, 0)
         assert first_obj == second_obj
