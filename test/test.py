36e5cbdc96c1f7045edd4c18ed4d73c302bcbad3
vkpatil <vishalvkpatil8@gmail.com>
2022-02-07 11:46:17 +0530

    [vishal] Updated: test case for checking null values for inch unit

--- test/test.py
+++ test/test.py
@@ -1,3 +1,10 @@
+"""
+@Author: Vishal Patil
+@Date: 05-02-2022 11-00-00
+@Last Modified by: Vishal Patil
+@Last Modified time: 07-02-2022 11:50:00
+@Title : solve test case 1.7
+"""
 from feet import Feet
 from inch import Inch
 from quantity_measurement import QuantityMeasurement
@@ -8,7 +15,6 @@ class Test:
     def test_compare_two_having_zero_values(self):
         """
             desc: check two object using equal method having zero length
-
         """
         first_obj = QuantityMeasurement(Feet.FEET, 0)
         second_obj = QuantityMeasurement(Feet.FEET, 0)
@@ -20,6 +26,14 @@ class Test:
         """
         first_obj = QuantityMeasurement(Feet.FEET, 1)
         second_obj = QuantityMeasurement(Feet.FEET, 1)
+        assert first_obj == second_obj
+
+    def test_for_checking_reference(self):
+        """
+            desc: check object using equal method have same reference
+        """
+        first_obj = QuantityMeasurement(Feet.FEET, 0)
+        second_obj = first_obj
         assert first_obj == second_obj
 
     def test_raise_exception_on_different_type_values(self):
@@ -36,14 +50,6 @@ class Test:
         """
         first_obj = QuantityMeasurement(Feet.FEET, 1)
         second_obj = QuantityMeasurement(Feet.FEET, 1)
-        assert first_obj == second_obj
-
-    def test_for_checking_reference(self):
-        """
-            desc: check object using equal method have same reference
-        """
-        first_obj = QuantityMeasurement(Feet.FEET, 0)
-        second_obj = QuantityMeasurement(Feet.FEET, 0)
         assert first_obj == second_obj
 
     def test_compare_two_having_zero_values_for_inch_unit(self):
